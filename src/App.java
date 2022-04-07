675d3fef7b5ae56443d547e4fb0c30032d59afef
mahyarfardin <mahyarfardinfar@gmail.com>
2022-04-07 22:28:01 +0430

    done students part

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import People.Student;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner=new Scanner(System.in);
+
+        ArrayList<Student> students=new ArrayList<>();
+
+        System.out.println("How many Students: ");
+        int t=scanner.nextInt();
+
+        for(int i=0; i<t; i++){
+            System.out.println("Adding Student\n");
+            Student student=new Student();
+            students.add(student);
+        }
+
+        for (Student s : students) {
+            s.Info();
+        }
     }
 }
