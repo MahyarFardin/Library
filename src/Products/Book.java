c8431aac0b8764ce7025d0ccaccf0a0059295100
mahyarfardin <mahyarfardinfar@gmail.com>
2022-04-20 20:51:24 +0430

    done with book

--- src/Products/Book.java
+++ src/Products/Book.java
@@ -24,7 +24,7 @@ public class Book implements IBook {
         this.publisher = "";
         this.ganre = EGanre.UD;
         this.authors = new ArrayList<Author>();
-        this.publishedDate=null;
+        this.publishedDate = null;
         this.page = 0;
         this.code = 0;
         this.id = 0;
@@ -39,7 +39,7 @@ public class Book implements IBook {
             EGanre ganre,
             ArrayList<Author> authors,
             int page,
-            Calendar publishedDate,
+            Date publishedDate,
             int code,
             ELangeANDNationality lang) {
 
@@ -56,7 +56,88 @@ public class Book implements IBook {
         this.id = idGenerator;
     }
 
-    public Book (Book book){
-        this(book.title, book.publisher, book.ganre, new ArrayList<>(book.authors), book.page, book.publishedDate, book.code, book.lang);
+    public Book(Book book) {
+        this(book.title, book.publisher, book.ganre, new ArrayList<>(book.authors), book.page, book.publishedDate,
+                book.code, book.lang);
+    }
+
+    public void setAuthors(ArrayList<Author> authors) {
+        this.authors = authors;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setGanre(EGanre ganre) {
+        this.ganre = ganre;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static void setIdGenerator(int idGenerator) {
+        Book.idGenerator = idGenerator;
+    }
+
+    public void setLang(ELangeANDNationality lang) {
+        this.lang = lang;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<Author> getAuthors() {
+        return authors;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public EGanre getGanre() {
+        return ganre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getIdGenerator() {
+        return idGenerator;
+    }
+
+    public ELangeANDNationality getLang() {
+        return lang;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
